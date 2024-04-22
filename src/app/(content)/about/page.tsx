c699f03fc197a27f0786c3152c540dfb33e75b76
Rickard Sandgren <Rickard.Sandgren@cybercom.com>
2024-04-22 12:08:36 +0200

    outcast generated content for 'about' page

--- src/app/(content)/about/page.tsx
+++ src/app/(content)/about/page.tsx
@@ -1,27 +1,30 @@
+import markdownToHtml from '@/lib/markdownToHtml';
+import { getDocumentBySlug, getCollections, getDocuments, getDocumentPaths } from 'outstatic/server'
 
-
-import { getDocumentBySlug, getDocuments } from 'outstatic/server'
 
 
 async function getData() {
-  const post = getDocumentBySlug('pages', 'about');
+  const post = getDocumentBySlug('pages', 'about', [
+    "title",
+    "slug",
+    "content",
+  ]);
 
-  return post;
+  if (post != null) {
+    const content = await markdownToHtml(post.content)
+    return content;
+  }
+
+  return "";
 }
 
 export default async function About() {
   const post = await getData();
 
   return (
-    <>
-      At Munamii Cakery, each cake, each box of cupcakes that we make has a very special
-      value for us. We not only want to bring a dessert to your table but rather a unique
-      experience for you, which will become an unforgettable memory. We have been in the
-      pastry market for more than 8 years and now we offer you a new way to shop through
-      our website.
-      <div>
-        <h1 key={post?.slug}>{post?.title}</h1>
-      </div>
-    </>
+    <div
+      className="prose lg:prose-xl"
+      dangerouslySetInnerHTML={{ __html: post }}
+    />
   );
 }
